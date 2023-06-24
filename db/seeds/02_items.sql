845a1a3874482d2934797f1b7e39e854747fcfdc
eunsookim1 <92eunsoo@gmail.com>
2023-06-23 19:49:22 -0700

    add items seeds for items table

---
+++ db/seeds/02_items.sql
@@ -0,0 +1,12 @@
+INSERT INTO items
+(id, name, price, status, description, seller_id, date, photo)
+VALUES
+(1, 'The Victorian Styled Gold Mirror', 1890, true, 'This mirror is a beautiful mirror that shows excellent craftsmanship. A true one in a kind...', 1, '2023-05-20', 'https://cdn.shopify.com/s/files/1/0780/3843/products/JamesGoldFullLengthWEBUGC_1400x.jpg?v=1645787317'),
+
+(2, 'Henry the Great', 2500, true, 'A tresure to have in your livingroom...', 1, '2023-05-21', 'https://cdn.shopify.com/s/files/1/0612/4453/9106/products/72_10ddb8d7-1b6d-426c-a31f-eec888359e9f_1080x.jpg?v=1647309400'),
+
+(3, 'Lovely Diana', 4500, true, 'The mirror that was once owned by princess Diana...', 1, '2023-06-10', 'https://cdn.shopify.com/s/files/1/0612/4453/9106/products/Untitleddesign_5_1080x.jpg?v=1677133130'),
+
+(4, 'Boho Mirror', 180, true, 'A boho bamboo mirror that will bring warmth into your bathroom decor!', 1, '2023-06-20', 'https://cdn.shopify.com/s/files/1/0612/4453/9106/products/59_b6ede3d9-1921-46b2-8b4d-6fc63fe10467_1080x.jpg?v=1647309312'),
+
+(5, 'Round Rattan Mirror', 100, true, 'A simple but elegant round mirror for your hallway...', 1, '2023-06-22', 'https://cdn.shopify.com/s/files/1/0612/4453/9106/products/MedelRoundRattanMirror24Inch_f1123f45-0fa1-4cb2-b413-90d7efeffef4_1080x.jpg?v=1637101983');
