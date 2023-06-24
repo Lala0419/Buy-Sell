6d0dcb35c6a9ace940886cf44ac09374ea0e72a2
eunsookim1 <92eunsoo@gmail.com>
2023-06-24 02:04:33 -0700

    add get request for items

--- routes/items.js
+++ routes/items.js
@@ -2,17 +2,29 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/connection");
 
-router.get("/", (req, res) => {
-	const query = `SELECT * FROM widgets`;
-	console.log(query);
-	db.query(query)
-		.then((data) => {
-			const widgets = data.rows;
-			res.json({ widgets });
-		})
-		.catch((err) => {
-			res.status(500).json({ error: err.message });
-		});
+// Original template:
+// router.get("/", (req, res) => {
+// 	const query = `SELECT * FROM widgets`;
+// 	console.log(query);
+// 	db.query(query)
+// 		.then((data) => {
+// 			const widgets = data.rows;
+// 			res.json({ widgets });
+// 		})
+// 		.catch((err) => {
+// 			res.status(500).json({ error: err.message });
+// 		});
+// });
+
+// module.exports = router;
+
+router.get("/items", (req, res) => {
+  db.getAllItems(req.query, 9)
+    .then((items) => res.send({ items }))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.message });
+    });
 });
 
 module.exports = router;
