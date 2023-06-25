b23e0780544e23fd30a594816c9dd437d67a0003
eunsookim1 <92eunsoo@gmail.com>
2023-06-24 22:04:42 -0700

    add get requests for homepage

--- routes/items.js
+++ routes/items.js
@@ -21,7 +21,8 @@ const itemQueries = require(`../db/queries/items`);
 // according to server.js this route is actually localhost8080/items
 router.get("/", (req, res) => {
   itemQueries
-    .getAllItems()
+    .getAllItems(req.query)
+    // we are using query because we're trying to filter
     .then((items) => res.json(items))
     // we want it as a string the 'items' for res.json as I'm sending an array
     // when i'm working on the app always send as res.json
@@ -33,7 +34,7 @@ router.get("/", (req, res) => {
 
 router.get("/new_items", (req, res) => {
   itemQueries
-    .getNewItems(req, 3)
+    .getNewItems()
     .then((items) => res.json(items))
     .catch((err) => {
       console.error(err);
