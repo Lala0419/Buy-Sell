c257c2bf377edbd31355280c5e18b8ce83e6bfc3
eunsookim1 <92eunsoo@gmail.com>
2023-06-26 14:55:51 -0700

    change css for new items, update filter jquery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,73 @@
 // Client facing scripts here
 
+$(() => {
 
+  $(".filter").click(() => {
+    const min_price = parseInt($('#min').val())
+    const max_price = parseInt($('#max').val());
+
+    if (min_price || max_price) {
+      $.ajax({
+        type: 'GET',
+        url: '/items',
+        data: {
+          "min_price": min_price,
+          "max_price": max_price
+        },
+      })
+      .done((response) => {
+        console.log(response);
+        const $itemsList = $('.items_container');
+        $itemsList.empty();
+
+        for(const item of response) {
+         $itemsList.append(getItemRow(item))
+        }
+      });
+    }
+
+    console.log(min_price, max_price)
+  })
+
+  const getItemRow = (item) => {
+    const $item = `
+      <article class="item">
+      <span>
+        <img class="item_photo" alt="item-photo" src=${item.photo}/>
+      </span>
+      <span class="name_price">
+        <h3 class="item_name">${item.name}</h3>
+        <p class="item_price">${item.price} CAD</p>
+      </span>
+
+      </article>
+      `
+      return $item;
+  }
+
+  const getAllMirrors = () => {
+    $.ajax({
+      type: 'GET',
+      url: '/items'
+    }) // need an event handler or a callback once done calling the function
+    .done((response) => {
+      console.log(response);
+      const $itemsList = $('.items_container');
+      $itemsList.empty();
+
+      // // I want to use query to fetch the items not a loop. is it possible?
+      for(const item of response) {
+       $itemsList.append(getItemRow(item))
+      }
+    });
+  }
+  getAllMirrors();
+})
+
+// Where does my ajax' get request go? or the html?
+
+//.load(".items_container")
+/*
 $(() => {
   // min & max boxes should be empty?
   $('#filter').on('click', () => {
@@ -48,3 +115,4 @@ const $newItem = `
   </span>
 </article>
 `
+*/
