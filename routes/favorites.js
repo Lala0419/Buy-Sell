a66bfe73ce961925be33273d391ae0deb5127c87
AliyaJK <kanani.aliya@gmail.com>
2023-06-27 16:45:26 +0000

    add comment

--- routes/favorites.js
+++ routes/favorites.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const db = require("../db/connection");
 
 router.get("/", (req, res) => {
-  // const userId = req.user.id; // <-- not sure if that's right
+  // const userId = req.user.id; // <-- not sure if that's right..
   const userID = 1;
 
   db.query("SELECT * FROM favourites WHERE user_id = $1", [userID])
