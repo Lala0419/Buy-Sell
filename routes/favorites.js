b944551481304b7ff03f1da641c63cc64b3f6c49
AliyaJK <kanani.aliya@gmail.com>
2023-06-27 12:50:22 +0000

    fix bug in favorites route

--- routes/favorites.js
+++ routes/favorites.js
@@ -6,10 +6,11 @@ router.get("/", (req, res) => {
   // const userId = req.user.id; // <-- not sure if that's right
   const userID = 1;
 
-  db.query("SELECT * FROM favorites WHERE user_id = $1", [userID])
+  db.query("SELECT * FROM favourites WHERE user_id = $1", [userID])
     .then((data) => {
       const favoriteItems = data.rows;
-      res.json({ favoriteItems });
+      // res.json({ favoriteItems });
+      res.render("favorites", { favoriteItems });
     })
     .catch((err) => {
       res.status(500).json({ error: err.message });
