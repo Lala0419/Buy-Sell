1d4ce9e29f2651943de6b13d9f06de7ebea53a82
Nanaya <nanayamiki@gmail.com>
2023-06-28 12:40:00 -0700

    add nav bar on message page

--- routes/messages.js
+++ routes/messages.js
@@ -2,28 +2,40 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/connection");
 const messageQueries = require("../db/queries/messages");
+const userQueries = require("../db/queries/users");
 
 /////////////////////////////////////////
 //  GET MESSAGES (reciever)
 /////////////////////////////////////////
 
 router.get("/", (req, res) => {
-	//res.render("messages");
+	const userId = req.cookies.userId;
 	const reciever_id = req.body.user_id;
+	//do they NOT have a cookie?
+	if (!userId) {
+		return res.status(401).send("you must login first");
+	}
 
-	return messageQueries
-		.getMessage(reciever_id)
-		.then((data) => {
-			console.log("data", data);
-			let templateVars = {
-				messages: data,
-			};
-			res.render("messages", templateVars);
-			//return res.json({ data });
-		})
-		.catch((err) => {
-			res.status(500).json({ error: err.message });
-		});
+	//get a single use obj
+	userQueries.getSingleUser(userId).then((user) => {
+		console.log("user", user);
+
+		return messageQueries
+			.getMessage(reciever_id)
+			.then((data) => {
+				console.log("data", data);
+				const templateVars = {
+					messages: data,
+					user: user,
+				};
+				res.render("messages", templateVars);
+				//return res.json({ data });
+			})
+			.catch((err) => {
+				res.status(500).json({ error: err.message });
+			});
+	});
+	//res.render("messages");
 });
 
 /////////////////////////////////////////
